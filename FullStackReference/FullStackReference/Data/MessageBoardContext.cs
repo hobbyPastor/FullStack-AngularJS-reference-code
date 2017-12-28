0f48aa18b440f4bf99fcb4b0f443de04b28fd818
BERRUM Kristian <kristian.berrum@live.com>
2017-12-28 19:35:59 +0100

    Added Context class for EF.

---
+++ FullStackReference/FullStackReference/Data/MessageBoardContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FullStackReference.Data
+{
+    public class MessageBoardContext: DbContext
+    {
+        public MessageBoardContext() : base("DefaultConnection")
+        {
+            
+        }
+
+        public DbSet<Topic> Topics { get; set; }
+        public DbSet<Reply> Replies { get; set; }
+    }
+}
