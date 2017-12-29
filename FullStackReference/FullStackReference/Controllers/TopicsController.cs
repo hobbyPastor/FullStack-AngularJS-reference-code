0857326b741e9728fdaf173d3ccef836337d97e9
BERRUM Kristian <kristian.berrum@live.com>
2017-12-29 13:38:31 +0100

    Added WebApi to Project for REST integration

---
+++ FullStackReference/FullStackReference/Controllers/TopicsController.cs
@@ -0,0 +1,28 @@
+﻿using FullStackReference.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace FullStackReference.Controllers
+{
+    public class TopicsController : ApiController
+    {
+        private IMessageBoardRepository _repo;
+
+        public TopicsController(IMessageBoardRepository repo)
+        {
+            _repo = repo;
+        }
+        public IEnumerable<Topic> Get()
+        {
+            List<Topic> topics =_repo.GetTopics()
+                .OrderByDescending(t => t.Created)
+                .Take(50)
+                .ToList();
+            return topics;
+        }
+    }
+}
