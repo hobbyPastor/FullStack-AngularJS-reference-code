a61b7561ccf54fb95fe6454af4b75fbce5df104d
Kristian Berrum <kristian.berrum@live.com>
2018-01-03 08:03:02 +0100

    Not complete fix of routing.

--- FullStackReference/FullStackReference/Js/home-index.js
+++ FullStackReference/FullStackReference/Js/home-index.js
@@ -1,19 +1,28 @@
 ﻿//home-index.js
-angular.module('app', [])
-    .controller('homeIndexController', function ($scope, $http) {
-        $scope.data = [];
-        $scope.isBusy = true;
+angular.module('homeIndex', []);
 
-        $http.get("/api/v1/topics?includeReplies=true")
-            .then(function (result) {
-                // success
-                angular.copy(result.data, $scope.data);
-            },
-            function () {
-                // error
-                alert("could not load topics");
-            })
-            .then(function () {
-                $scope.isBusy = false;
-            });
+homeIndex.config(function ($routeProvider) {
+    $routeProvider.when("/", {
+        controller: "TopicsController",
+        templateUrl: "/Template/topicsView.html"
     });
+    $routeProvider.otherwise({ redirectTo: "/" });
+});
+
+homeIndex.controller('homeIndexController', function ($scope, $http) {
+    $scope.data = [];
+    $scope.isBusy = true;
+
+    $http.get("/api/v1/topics?includeReplies=true")
+        .then(function (result) {
+            // success
+            angular.copy(result.data, $scope.data);
+        },
+        function () {
+            // error
+            alert("could not load topics");
+        })
+        .then(function () {
+            $scope.isBusy = false;
+        });
+});
